56264d5981129c6a87c02b11151a7dd640bcdd10
Ravy Freitas <ravy.freitas@meta3.com.br>
2019-06-13 13:51:53 -0300

    Css header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,8 @@ export default function Header() {
     return (
         <header id='main-header'>
             <div className="main-content">
-                <img src="" alt="Aquinos"/>
-                <img src="" alt="Novo"/>
+                <img style={{width:"5%"}} src="https://cdn0.iconfinder.com/data/icons/foody-icons/32/FoodyIcons_color-06-512.png" alt="Aquinos"/>
+                <img style={{width:"5%"}} src="https://cdn0.iconfinder.com/data/icons/foody-icons/32/FoodyIcons_color-06-512.png" alt="Novo"/>
             </div>
         </header>
     );
