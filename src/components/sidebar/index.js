bf0a015ea1ce034bac49933927ebf9d56d23a274
klenner1 <klenne.al@gmail.com>
2019-06-16 19:07:11 -0300

    SideBar

---
+++ src/components/sidebar/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import SidebarCategory from '../sidebarCategory';
+import './sideBar.css';
+class SideBar extends Component {
+
+    render() {
+        return (
+            <>
+                <div>
+                    <div className={`sidebar fixed sn-bg-1`}>
+                        <ul>
+                            <SidebarCategory></SidebarCategory>
+                        </ul>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
+export default SideBar;
--- src/main/App.js
+++ src/main/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from '../routes';
 import Header from '../components/header';
-
+import SideBar from '../components/sidebar';
 
 function App() {
   return (
-    <BrowserRouter forceRefresh='false' >
-    
-    <Header/>
-      <Routes />
-    </BrowserRouter>
+    <BrowserRouter forceRefresh={false} >
+      <Header />
+      <SideBar />
+      <div className="app-content">
+      <Routes  />
+      </div>
+    </BrowserRouter >
 
   );
 }
