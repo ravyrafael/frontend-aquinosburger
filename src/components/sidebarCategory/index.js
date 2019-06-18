cb6a779ca13a526deb160cc10032523f0ef86062
Ravy <ravyrafael@gmail.com>
2019-06-18 15:47:51 -0300

    alteração na index

--- src/components/sidebarCategory/index.js
+++ src/components/sidebarCategory/index.js
@@ -3,14 +3,19 @@ import { Row, Col } from 'react-bootstrap';
 import { routes } from '../../Utils/routeUtils.js'
 
 class SidebarCategory extends Component {
+    state = {
+        routes,
+        active: "dashboard"
+    }
 
     render() {
         return (
             <>
                 {routes.map(item => {
+
                     return (
                         <li key={item.name}>
-                            <a className="sidebar-category container-fluid active" href={item.path}>
+                            <a className="sidebar-category container-fluid active" onClick={()=>{this.setState({active:item.name})}} href={item.path}>
                                 <Row className="nowrap">
                                     <Col xs="auto"><div className="sidebar-category-icon">{item.icone()}</div></Col>
                                     <Col className="sidebar-category-text">
